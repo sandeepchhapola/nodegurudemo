f1b25e2bf374c6e9dbecbd317ab42057c4c5c660
sandeepchhapola <schhapola0001@gmail.com>
2014-11-24 17:37:29 +0530

    code moved from server to urlMapping

--- configurations/urlMappings.js
+++ configurations/urlMappings.js
@@ -1,4 +1,30 @@
 module.exports.initUrlMappings=function(){
+    var viewEngine = require("ejs-locals"),
+        methodOverride = require('method-override'),
+        session = require('express-session'),
+        bodyParser = require('body-parser'),
+        multer = require('multer'),
+        errorHandler = require('errorhandler');
+
+    // all environments
+    _app.set('port', process.env.PORT || 3000);
+    _app.set('views', _path.join(_appBaseUrl, "views"));
+    _app.engine('ejs', viewEngine);
+    _app.set('view engine', 'ejs');
+    _app.use(methodOverride());
+    _app.use(session({ resave: true,
+        saveUninitialized: true,
+        secret: 'uwotm8' }));
+    _app.use(bodyParser.json());
+    _app.use(bodyParser.urlencoded({ extended: true }));
+    _app.use(multer());
+    _app.use(_express.static(_path.join(_appBaseUrl, 'client')));
+
+    // error handling middleware should be loaded after the loading the routes
+    if ('development' == _app.get('env')) {
+        _app.use(errorHandler());
+    }
+
     _app.get("/userlist",_Controllers.userController.getUserList);
     _app.get("/",function(req,res){
         res.render("index");
--- server/server.js
+++ server/server.js
@@ -1,36 +1,12 @@
-var serverSetup = require('../stuff/tasks/serverSetup'),
-    viewEngine = require("ejs-locals"),
-    methodOverride = require('method-override'),
-    session = require('express-session'),
-    bodyParser = require('body-parser'),
-    multer = require('multer'),
-    errorHandler = require('errorhandler');
+var serverSetup = require('../stuff/tasks/serverSetup');
 
 serverSetup.initServerSetup();
-
-// all environments
-_app.set('port', process.env.PORT || 3000);
-_app.set('views', _path.join(_appBaseUrl, "views"));
-_app.engine('ejs', viewEngine);
-_app.set('view engine', 'ejs');
-_app.use(methodOverride());
-_app.use(session({ resave: true,
-    saveUninitialized: true,
-    secret: 'uwotm8' }));
-_app.use(bodyParser.json());
-_app.use(bodyParser.urlencoded({ extended: true }));
-_app.use(multer());
-_app.use(_express.static(_path.join(_appBaseUrl, 'client')));
-
-// error handling middleware should be loaded after the loading the routes
-if ('development' == _app.get('env')) {
-    _app.use(errorHandler());
-}
 
 var _server = _http.createServer(_app)
     .on("error", function (err) {
         console.log(err);
     });
+
 _server.listen(3000, function () {
     console.log('Server listening on port', _server.address().port);
 })
